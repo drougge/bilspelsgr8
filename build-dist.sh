91856399283392e86d218f919c51cf0df05d0c5f
Carl Drougge <bearded@longhaired.org>
2014-07-12 18:51:35 +0200

    Packaging and docs.

---
+++ build-dist.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm -f bilspelsgr8.tar.gz
+cd ..
+tar zcf bilspelsgr8/bilspelsgr8.tar.gz bilspelsgr8/README* bilspelsgr8/CREDITS* bilspelsgr8/run* bilspelsgr8/game/*.py bilspelsgr8/game/*.png bilspelsgr8/game/*.wav bilspelsgr8/game/*.yaml bilspelsgr8/game/*.goals bilspelsgr8/game/*.cars bilspelsgr8/game/*.towers
+
