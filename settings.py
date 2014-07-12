327ee4187c03f10b6f9ae1102f389a5f36bb1f87
Carl Drougge <bearded@longhaired.org>
2014-07-12 14:57:45 +0200

    As much unicode sanity as pygame will allow.

--- settings.py
+++ settings.py
@@ -1,8 +1,4 @@
 import yaml
-def custom_str_constructor(loader, node):
-  return loader.construct_scalar(node).encode('utf-8')
 
-yaml.add_constructor(u'tag:yaml.org,2002:str', custom_str_constructor)
-
-with open('settings.yaml') as f: 
+with open('settings.yaml') as f:
 	settings = yaml.load(f)
